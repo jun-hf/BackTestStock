9ee1346aac9cae577baab862f5ea48c843958ebc
jun-hf <86782267+jun-hf@users.noreply.github.com>
2023-12-10 15:44:06 +0000

    Created test

--- test/expectedStockList.js
+++
@@ -1,8 +0,0 @@
-[
-    {
-        timeSeries: 'DAILY',
-        symbol: 'IBM',
-        date: 2023-12-08T00:00:00.000Z,
-        openPrice: 8,
-    }
-]
--- test/lib/getStock.test.ts
+++ test/lib/getStock.test.ts
@@ -1,11 +1,14 @@
 import {
     getStock,
-    _parseTimeSeriesKey
+    _parseTimeSeriesKey,
+    buildStockList,
+    getStockList
 } from '../../src/lib/getStockList';
 import axios from 'axios';
-import IBMDaily from '../IBMDaily.json';
+import IBMDaily from '../fixture/IBMDaily.json';
+import expectedStockList from '../fixture/expectedStockList';
 
-import { expect, test, beforeAll, describe, it, vi, afterEach} from 'vitest';
+import { expect, beforeAll, describe, it, vi, afterEach} from 'vitest';
 
 const symbol = 'IBM';
 const timeSeries = 'DAILY';
@@ -20,13 +23,29 @@ describe('::getStockList', () => {
     afterEach(() => {
         vi.resetAllMocks();
     });
+    describe(':getStockList', () => {
+        afterEach(() => {
+            vi.resetAllMocks();
+        });
+        it('should build correct stock list', async () => {
+            const result = await getStockList(symbol, timeSeries);
+            expect(result).toStrictEqual(expectedStockList);
+        })
+        it('should return [], when failed to build stock', async () => {
+            const mock = vi.fn().mockImplementation(getStock);
+            mock.mockImplementationOnce(() => {throw new Error()});
+            const result = await getStockList('INVALID', 'INVALIDSYMBOL');
+            expect(result).toStrictEqual([]);
+        });
+    })
     it(':getStock -> request the correct url to AlphaVantage', async () => {
         const expectedUrl = `https://www.alphavantage.co/query?function=TIME_SERIES_${timeSeries}&symbol=${symbol}&apikey=demo`;
         await getStock(symbol, timeSeries);
         expect(spy).toHaveBeenCalledWith(expectedUrl);
     });
-    it(':builStockList', async () => {
-        
+    it(':builStockList -> should construct the correct stock list', async () => {
+        const result = await buildStockList(IBMDaily.data);
+        expect(result).toStrictEqual(expectedStockList);
     });
     it(':_parseTimeSeriesKey', () => {
         const expectedDaily = 'Time Series (Daily)';
@@ -36,5 +55,5 @@ describe('::getStockList', () => {
 
         expect(daily).toBe(expectedDaily);
         expect(monthly).toBe(expectedMonthly);
-    })
+    });
 })
