e136ced9636ff1ae66c02899e6e12b41fa120f1e
jun-hf <86782267+jun-hf@users.noreply.github.com>
2023-12-14 22:17:05 +0000

    Create api.ts and buySellController

---
+++ api.ts
@@ -0,0 +1,22 @@
+import {
+  BuySell
+} from 'BuySell';
+
+constructor({symbol, timeSeries, buyRate, sellRate, amount}: BuySellRequirement) {
+        this.stockList = [];
+
+const buySellController = async (req) => {
+  try 
+  const buySell = new BuySell(req);
+  await buySell.init();
+  const stockList = buySell.getStockList();
+  const netWorth = buySell.getNetWorth();
+  const profit = buySell.getProfit();
+  return {
+    stockList, 
+    netWorth,
+    profit,
+  };
+  throw 
+    how should we process this.
+};
