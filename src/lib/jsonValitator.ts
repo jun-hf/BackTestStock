c20b4191dca0399b56ff60bfbcc672375b5c0efb
jun-hf <86782267+jun-hf@users.noreply.github.com>
2023-12-18 23:44:48 +0000

    Completed json validator

---
+++ src/lib/jsonValitator.ts
@@ -0,0 +1,21 @@
+import Ajv from 'ajv';
+import type { Request, Response, NextFunction } from 'express';
+import { JsonSchema } from '../types/utilType'
+
+const ajv = new Ajv();
+
+export const jsonValidator = (inputSchema: JsonSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const validate = ajv.compile(inputSchema);
+        const data = req.body;
+        const valid = validate(data);
+        if (!valid) {
+            if (validate.errors) {
+                const errorMessage = validate.errors[0].message;
+                console.error(errorMessage)
+                return res.status(400).send(errorMessage);
+            };
+        };
+        next();
+    };
+};
