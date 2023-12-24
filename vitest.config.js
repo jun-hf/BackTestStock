22b4ab4dc9a1dbd6581184aa6c06de86bcb1f8a5
jun-hf <86782267+jun-hf@users.noreply.github.com>
2023-12-24 12:03:01 +0000

    update package version

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ const { configDefaults, defineConfig } = require('vitest/config');
 
 export default defineConfig({
   test: {
+    globals: true,
     include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     exclude: [...configDefaults.exclude, '**/frontend']
     },
