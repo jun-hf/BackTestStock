3bf101da8e3f5b3fa75c58c9c2bcfbec2eb25389
jun-hf <fong.junh@gmail.com>
2023-12-27 17:28:12 +0000

    Change app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import BuySellForm from './BuySellForm';
+import DashboardExample from './DashBoard';
 import { Title } from "@tremor/react";
 
 function App() {
   return (
-    <main className="p-4 md:p-10 mx-auto max-w-7xl">
+    <div className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Backtest Stock</Title>
       <BuySellForm />
-    </main>
+    </div>
   )
 }
 
